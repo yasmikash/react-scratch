9a08a71f342250d2bea71742e39cfdd5e1466667
Yasmika Saubhagya <it18209280@my.sliit.lk>
2021-12-03 21:17:49 -0800

    Init the app

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Body from "./Body";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Hello, React!</h1>
+      <Body />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Body.css";
+
+const Body = () => {
+  return (
+    <div className="content header">
+      <h2>This is the body</h2>
+    </div>
+  );
+};
+
+export default Body;
